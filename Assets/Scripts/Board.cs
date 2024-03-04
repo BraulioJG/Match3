85d13f24cbe6c5392ded262a75a1f1b19d7fa47a
Braulio Jiménez García <92713556+BraulioJG@users.noreply.github.com>
2024-03-04 18:55:17 +0100

    Deslizamiento de piezas
    Implementado el deslizamiento de las piezas, limitado a una casilla como máximo en X o Y.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
@@ -13,11 +14,37 @@ public class Board : MonoBehaviour
     public float cameraSizeOffset;
     public float cameraVerticalOffset;
 
+    public GameObject[] availablePieces;
+
+    Tile[,] tiles;
+    Piece[,] pieces;
+
+    Tile startTile;
+    Tile endTile;
+
     // Start is called before the first frame update
     void Start()
     {
+        tiles = new Tile[width, height];
+        pieces = new Piece[width, height];
         SetupBoard();
         PositionCamera();
+        SetupPieces();
+    }
+
+    private void SetupPieces()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
+                var o = Instantiate(selectedPiece, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+                pieces[x, y] = o.GetComponent<Piece>();
+                pieces[x, y]?.Setup(x, y, this);
+            }
+        }
     }
 
     private void SetupBoard()
@@ -28,6 +55,8 @@ public class Board : MonoBehaviour
             {
                 var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                tiles[x,y] = o.GetComponent<Tile>();
+                tiles[x,y]?.Setup(x, y, this);
             }
         }
     }
@@ -43,9 +72,46 @@ public class Board : MonoBehaviour
         Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset : vertical;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TileDown(Tile tile_)
     {
+        startTile = tile_;
+    }
 
+    public void TileOver(Tile tile_)
+    {
+        endTile = tile_;
+    }
+
+    public void TileUp(Tile tile_)
+    {
+        if (startTile != null && endTile != null && IsCloseTo(startTile, endTile))
+        {
+            SwapTiles();
+        }
+    }
+
+    private void SwapTiles()
+    {
+        var StartPiece = pieces[startTile.x, startTile.y];
+        var EndPiece = pieces[endTile.x, endTile.y];
+
+        StartPiece.Move(endTile.x, endTile.y);
+        EndPiece.Move(startTile.x, startTile.y);
+
+        pieces[startTile.x,startTile.y] = EndPiece;
+        pieces[endTile.x, endTile.y] = StartPiece;
+    }
+
+    public bool IsCloseTo(Tile start, Tile end)
+    {
+        if(Math.Abs(start.x - end.x) == 1 && start.y == end.y)
+        {
+            return true;
+        }
+        if (Math.Abs(start.y - end.y) == 1 && start.x == end.x)
+        {
+            return true;
+        }
+        return false;
     }
 }
